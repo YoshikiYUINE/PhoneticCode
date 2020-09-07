a6a03e9dcf362978e7fc541269c2068e618c379d
YoshikiYUINE <yoshiki.yuine@gmail.com>
2020-09-07 12:55:38 +0000

    apply Android 10 [api level 29]

--- app/src/main/java/info/eniuy/phonetic_code/ConvertCallSignActivity.java
+++ app/src/main/java/info/eniuy/phonetic_code/ConvertCallSignActivity.java
@@ -4,7 +4,7 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
